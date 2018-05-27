602243378d0b1066f0bea54d4c536f71a92edf91
mata889 <dam_900@unitec.edu>
2018-05-27 12:00:26 -0600

    Primero

---
+++ src/avail/lists/registros/de/pkglong/variable/AvailListsRegistrosDeLongVariable.java
@@ -0,0 +1,29 @@
+
+package avail.lists.registros.de.pkglong.variable;
+/*
+Estimados estudiantes,
+
+Utilicen este link para subir el ejercicio iniciado en clase. Como recordatorio:
+
+Deberán traducir de pseudo código al lenguaje utilizado en su proyecto el ejercicio de popular avail list desarrollado durante la clase.
+Manejar todos los issues discutidos en clase para generar y cargar la lista en memoria (cargar metadata, popular la lista, ordenarla de acuerdo a la estrategia de ubicación de registros elegida),
+Saludos y feliz fin de semana.
+*/
+import java.io.File;
+import java.util.Scanner;
+
+
+public class AvailListsRegistrosDeLongVariable {
+
+    static Scanner sc=new Scanner(System.in);
+   
+    File f=new File("Registros.txt");
+    
+    
+    public static void main(String[] args) {
+        int menu;
+        
+        
+    }
+    
+}
